7b618d91afadcef464b71683f6003d7b15764a3d
FabPrez <giuseppefabio.preziosa@polimi.it>
2025-05-16 19:49:26 +0200

    first commit

---
+++ launch/run_grasp_node.launch.py
@@ -0,0 +1,19 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_prefix, get_package_share_directory
+
+def generate_launch_description():
+    pkg_prefix = get_package_share_directory('graspnet_ros')
+    venv_path = os.path.join(pkg_prefix, 'venv_graspnet')
+    python_bin = os.path.join(venv_path, 'bin', 'python')
+
+    return LaunchDescription([
+        Node(
+            package='graspnet_ros',
+            executable='main.py',  # questo deve essere il nome dell'entry point
+            name='graspnet_ros',
+            output='screen',
+            prefix=f'{python_bin}'
+        )
+    ])
